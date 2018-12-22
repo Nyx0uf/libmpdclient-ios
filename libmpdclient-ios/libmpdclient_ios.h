6a30d15ef8e610dc597303284b768a88e5aa8df9
Benjamin Godard <benjgodard@gmail.com>
2018-12-22 14:12:07 +0100

    first commit

---
+++ libmpdclient-ios/libmpdclient_ios.h
@@ -0,0 +1,12 @@
+//
+//  libmpdclient_ios.h
+//  libmpdclient-ios
+//
+//  Created by Benjamin on 22/12/2018.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libmpdclient_ios : NSObject
+
+@end
---
+++ libmpdclient-ios/libmpdclient_ios.m
@@ -0,0 +1,12 @@
+//
+//  libmpdclient_ios.m
+//  libmpdclient-ios
+//
+//  Created by Benjamin on 22/12/2018.
+//
+
+#import "libmpdclient_ios.h"
+
+@implementation libmpdclient_ios
+
+@end
